beabf748707d63d5d8f1acab366fe18496f1af3f
christian nickerson <christian_nickerson@hotmail.com>
2025-02-22 14:34:35 +0000

    lazygit neotree refresh added

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -8,5 +8,15 @@ return {
 	},
 	config = function()
 		vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
+
+		vim.api.nvim_create_autocmd({ "BufLeave" }, {
+			pattern = { "*lazygit*" },
+			group = vim.api.nvim_create_augroup("git_refresh_neotree", { clear = true }),
+			callback = function()
+				require("neo-tree.sources.filesystem.commands").refresh(
+					require("neo-tree.sources.manager").get_state("filesystem")
+				)
+			end,
+		})
 	end,
 }

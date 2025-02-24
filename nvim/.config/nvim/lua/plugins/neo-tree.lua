23bd81b2b267ad7e5163ab49f104253b29b83324
Christian Nickerson <christian.nickerson@faculty.ai>
2025-02-24 10:44:23 +0000

    show hidden files by default

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,22 +1,33 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "MunifTanjim/nui.nvim",
-    },
-    config = function()
-        vim.keymap.set("n", "<leader>t", ":Neotree toggle<CR>", { desc = "Neotree toggle" })
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+					hide_dotfiles = false,
+					hide_gitignored = true,
+					never_show = { ".git" },
+				},
+			},
+		})
 
-        vim.api.nvim_create_autocmd({ "BufLeave" }, {
-            pattern = { "*lazygit*" },
-            group = vim.api.nvim_create_augroup("git_refresh_neotree", { clear = true }),
-            callback = function()
-                require("neo-tree.sources.filesystem.commands").refresh(
-                    require("neo-tree.sources.manager").get_state("filesystem")
-                )
-            end,
-        })
-    end,
+		vim.keymap.set("n", "<leader>t", ":Neotree toggle<CR>", { desc = "Neotree toggle" })
+
+		vim.api.nvim_create_autocmd({ "BufLeave" }, {
+			pattern = { "*lazygit*" },
+			group = vim.api.nvim_create_augroup("git_refresh_neotree", { clear = true }),
+			callback = function()
+				require("neo-tree.sources.filesystem.commands").refresh(
+					require("neo-tree.sources.manager").get_state("filesystem")
+				)
+			end,
+		})
+	end,
 }

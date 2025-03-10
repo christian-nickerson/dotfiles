f5c11868e0859fcc2747df23b3696ba46c5ff0cc
christian nickerson <christian_nickerson@hotmail.com>
2025-03-10 09:58:02 +0000

    fromat tidy

--- nvim/.config/nvim/lua/plugins/debugging.lua
+++ nvim/.config/nvim/lua/plugins/debugging.lua
@@ -1,35 +1,37 @@
 return {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-        "rcarriga/nvim-dap-ui",
-        "nvim-neotest/nvim-nio",
-        "mfussenegger/nvim-dap-python",
-        "leoluz/nvim-dap-go",
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "rcarriga/nvim-dap-ui",
+            "nvim-neotest/nvim-nio",
+            "mfussenegger/nvim-dap-python",
+            "leoluz/nvim-dap-go",
+        },
+        config = function()
+            local dap, dapui = require("dap"), require("dapui")
+
+            require("dapui").setup()
+            require("dap-python").setup("python3")
+            require("dap-go").setup()
+
+            dap.listeners.before.attach.dapui_config = function()
+                dapui.open()
+            end
+
+            dap.listeners.before.launch.dapui_config = function()
+                dapui.open()
+            end
+
+            dap.listeners.before.event_terminated.dapui_config = function()
+                dapui.close()
+            end
+
+            dap.listeners.before.event_exited.dapui_config = function()
+                dapui.close()
+            end
+
+            vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { desc = "Debugger Toggle Breakpoint" })
+            vim.keymap.set("n", "<Leader>dc", dap.continue, { desc = "Debugger Continue" })
+        end,
     },
-    config = function()
-        local dap, dapui = require("dap"), require("dapui")
-
-        require("dapui").setup()
-        -- require("dap-python").setup("python3")
-        -- require("dap-go").setup()
-
-        dap.listeners.before.attach.dapui_config = function()
-            dapui.open()
-        end
-
-        dap.listeners.before.launch.dapui_config = function()
-            dapui.open()
-        end
-
-        dap.listeners.before.event_terminated.dapui_config = function()
-            dapui.close()
-        end
-
-        dap.listeners.before.event_exited.dapui_config = function()
-            dapui.close()
-        end
-
-        vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { desc = "Debugger Toggle Breakpoint" })
-        vim.keymap.set("n", "<Leader>dc", dap.continue, { desc = "Debugger Continue" })
-    end,
 }

34b9961def7eadff584200102cfca022bf5977a2
Christian Nickerson <christian.nickerson@faculty.ai>
2025-03-19 22:56:27 +0000

    neotest added

---
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,82 @@
+return {
+    "nvim-neotest/neotest",
+
+    dependencies = {
+        "nvim-neotest/nvim-nio",
+        "nvim-lua/plenary.nvim",
+        "antoinemadec/FixCursorHold.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-neotest/neotest-python",
+        "fredrikaverpil/neotest-golang",
+    },
+
+    keys = {
+        {
+            "<leader>nr",
+            "<cmd>lua require('neotest').run.run()<cr>",
+            desc = "Run nearest test",
+        },
+        {
+            "<leader>nf",
+            "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>",
+            desc = "Run current file",
+        },
+        {
+            "<leader>na",
+            "<cmd>lua require('neotest').run.run({ suite = true })<cr>",
+            desc = "Run all tests",
+        },
+        {
+            "<leader>nd",
+            "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>",
+            desc = "Debug nearest test",
+        },
+        {
+            "<leader>ns",
+            "<cmd>lua require('neotest').run.stop()<cr>",
+            desc = "Stop test",
+        },
+        {
+            "<leader>nn",
+            "<cmd>lua require('neotest').run.attach()<cr>",
+            desc = "Attach to nearest test",
+        },
+        {
+            "<leader>no",
+            "<cmd>lua require('neotest').output.open()<cr>",
+            desc = "Show test output",
+        },
+        {
+            "<leader>np",
+            "<cmd>lua require('neotest').output_panel.toggle()<cr>",
+            desc = "Toggle output panel",
+        },
+        {
+            "<leader>nt",
+            "<cmd>lua require('neotest').summary.toggle()<cr>",
+            desc = "Toggle summary",
+        },
+        {
+            "<leader>nc",
+            "<cmd>lua require('neotest').run.run({ suite = true, env = { CI = true } })<cr>",
+            desc = "Run all tests with CI",
+        },
+    },
+
+    config = function()
+        require("neotest").setup({
+            adapters = {
+
+                require("neotest-python")({
+                    dap = { justMyCode = false },
+                }),
+
+                require("neotest-golang")({
+                    dap = { justMyCode = false },
+                    args = { "-v" },
+                    runner = "go test",
+                }),
+            },
+        })
+    end,
+}

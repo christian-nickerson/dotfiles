04484b5b5dd895c33b87c3455ca1f98631af5da3
Christian Nickerson <christian_nickerson@hotmail.com>
2025-12-14 23:54:15 +0000

    tmux and nvim buffer transparency

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -2,24 +2,33 @@ return {
   "akinsho/bufferline.nvim",
   version = "*",
   event = "VeryLazy",
-  dependencies = "nvim-tree/nvim-web-devicons",
-  opts = {
-    options = {
-      mode = "buffers",
-      separator_style = "slant",
-      always_show_bufferline = false,
-      show_buffer_close_icons = false,
-      show_close_icon = false,
-      color_icons = true,
-      diagnostics = "nvim_lsp",
-      offsets = {
-        {
-          filetype = "neo-tree",
-          text = "File Explorer",
-          highlight = "Directory",
-          text_align = "left",
+  dependencies = { "nvim-tree/nvim-web-devicons", "catppuccin/nvim" },
+  config = function()
+    require("bufferline").setup({
+      options = {
+        mode = "buffers",
+        separator_style = "slant",
+        always_show_bufferline = false,
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        color_icons = true,
+        diagnostics = "nvim_lsp",
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "File Explorer",
+            highlight = "Directory",
+            text_align = "left",
+          },
         },
       },
-    },
-  },
+      highlights = require("catppuccin.groups.integrations.bufferline").get_theme({
+        custom = {
+          all = {
+            fill = { bg = "#181926" }, -- crust - darker than tmux status bar
+          },
+        },
+      }),
+    })
+  end,
 }
--- nvim/.config/nvim/lua/plugins/colourscheme.lua
+++ nvim/.config/nvim/lua/plugins/colourscheme.lua
@@ -5,15 +5,28 @@ return {
   lazy = false,
   priority = 999,
 
-  opts = {
-    integrations = {
-      treesitter = true,
-      treesitter_context = true,
-    },
-  },
+  opts = {},
 
   config = function()
-    require("catppuccin").setup({ flavour = "macchiato" })
+    require("catppuccin").setup({
+      flavour = "macchiato",
+      transparent_background = false,
+      integrations = {
+        treesitter = true,
+        treesitter_context = true,
+      },
+    })
     vim.cmd.colorscheme("catppuccin")
+
+    -- Make only the main buffer background transparent, text stays opaque
+    local transparent_groups = {
+      "Normal",
+      "NormalNC",
+      "SignColumn",
+      "EndOfBuffer",
+    }
+    for _, group in ipairs(transparent_groups) do
+      vim.api.nvim_set_hl(0, group, { bg = "NONE" })
+    end
   end,
 }
